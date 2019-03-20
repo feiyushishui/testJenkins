a782d0b8420922a3454899c708cfed466991f7a2
wangheng <wangheng@foxmail.com>
2019-03-20 11:07:52 +0800

    修改版2.0

--- demo4/src/main/java/com/tencent/gaio/workorder/controller/TestController.java
+++ demo4/src/main/java/com/tencent/gaio/workorder/controller/TestController.java
@@ -10,8 +10,7 @@ public class TestController {
 
     @RequestMapping(value = "/test", method = {RequestMethod.GET})
     public String create() {
-        return "demo4部署之后成功进入的页面";
+        return "测试demo4更新成功";
     }
-
 
 }
--- demo5/src/main/java/com/tencent/gaio/workorder/controller/TestController.java
+++ demo5/src/main/java/com/tencent/gaio/workorder/controller/TestController.java
@@ -5,12 +5,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @RestController
 public class TestController {
 
     @RequestMapping(value = "/test", method = {RequestMethod.GET})
     public String create() {
-        return "demo5部署之后成功进入的页面";
+        Date date = new Date();
+        String format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(date);
+        String s = "测试demo5项目更新，重新从git上面拉取并部署到服务器成功,时间是："+format;
+        return s;
     }
 
 
