be84e699058fdbcb1c6bdd814ad22a3afd456686
wangheng <wangheng@foxmail.com>
2019-03-20 14:12:10 +0800

    修改版2.2（测试任务里面的模块单独创建）

--- demo5/src/main/java/com/tencent/gaio/workorder/controller/TestController.java
+++ demo5/src/main/java/com/tencent/gaio/workorder/controller/TestController.java
@@ -15,7 +15,7 @@ public class TestController {
     public String create() {
         Date date = new Date();
         String format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(date);
-        String s = "测试不更新demo4，只更新demo5项目，重新从git上面拉取并部署到服务器成功,时间是："+format;
+        String s = "不更新demo4，只更新demo5项目，看看demo4有没有重新部署,时间是："+format;
         return s;
     }
 
